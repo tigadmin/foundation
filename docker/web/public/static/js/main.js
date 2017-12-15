5d3a4456bca46a4f751dea72880b8978cfad2db1
tigadmin <jason@techinvestmentgroup.com>
2017-12-15 16:58:49 -0500

    hope6

--- docker/web/public/static/js/main.js
+++ docker/web/public/static/js/main.js
@@ -22,7 +22,7 @@
         } else {
           html = songs.map(function(song) {
             return `
-    <article>
+    <!--<article>
                <v-card-media src="/static/img/local-business.jpg" height="200px">
                   </v-card-media>
                   <v-card-title primary-title>
@@ -34,7 +34,45 @@
                   <v-card-actions>
                     <v-btn flat color="teal">RATING: ${song.rating}</v-btn>
                     <router-link to="/localwebsitedesign/"><v-btn flat color="teal">Explore</v-btn></router-link>
-    </article>
+    </article>-->
+        <section>
+      <v-layout
+        column
+        wrap
+        class="my-5"
+        align-center
+      >
+        <v-flex xs12 sm8 class="my-3">
+          <div class="text-xs-center">
+            <h2 class="display-2 text-xs-center">Performance Based Local Marketing</h2>
+          </div>
+        </v-flex>
+        <v-flex xs12>
+          <v-container grid-list-xl>
+            <v-layout row wrap align-center>
+
+              <v-flex xs12 md4>
+                <v-card>
+
+                  <v-card-media src="/static/img/local-business.jpg" height="200px">
+                  </v-card-media>
+                  <v-card-title primary-title>
+                    <div>
+                      <h3 class="headline mb-0">Website Design</h3>
+                      <div class="subheading grey--text text--darken-1">Google powered website specifically designed for local business that works across all devices and loads instantly.</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn flat color="teal">PRICE: $999</v-btn>
+                    <router-link to="/localwebsitedesign/"><v-btn flat color="teal">Explore</v-btn></router-link>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </section>
                         `;
           }).join();
         }
