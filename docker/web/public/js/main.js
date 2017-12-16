75b2c53f445ab7de987da03906586ae3fd2c891d
tigadmin <jason@techinvestmentgroup.com>
2017-12-16 00:41:07 -0500

    hope7

--- docker/web/public/js/main.js
+++ docker/web/public/js/main.js
@@ -1,68 +1,65 @@
 (function() {
 
-    function loadHomepage() {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', '/api/contents?type=Song');
-        xhr.onreadystatechange = renderHomepage;
-        xhr.send(null);
-    }
+  function loadHomepage() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/contents?type=Song');
+    xhr.onreadystatechange = renderHomepage;
+    xhr.send(null);
+  }
 
-    function renderHomepage(event) {
-        const DONE = 4;
-        const OK = 200;
-        let xhr = event.currentTarget;
-        let html = '';
+  function renderHomepage(event) {
+    const DONE = 4;
+    const OK = 200;
+    let xhr = event.currentTarget;
+    let html = '';
 
-        if (xhr.readyState === DONE) {
-            if (xhr.status === OK) {
-                const songs = window.JSON.parse(xhr.responseText).data;
-                console.log(songs);
-                if(songs.length === 0){
-                    html = '<p><strong>There have not been any Songs added, <a href="/admin">go add some at /admin</a></strong></p>';
-                } else {
-                    html = songs.map(function(song) {
-                        return `
-                            <article>
-                              <v-container grid-list-md text-xs-center>
-    <v-layout flex row wrap>
-<div class="layout row wrap align-center" layout="row">
-<div class="card__text">
-<div class="layout">
-<div class="flex xs6 md12">
-<div class="card" data-ripple="false" style="height: auto;">
-
-<div class="card__media" style="height: 200px;">
-<div class="card__media__background" style="background: url(&quot;/static/img/local-business-5.jpg&quot;) center center / cover no-repeat;"></div>
-</div>
-
-<div class="card__title card__title--primary"><h3 class="headline mb-0">${song.title || 'Unknown'} by ${song.artist || 'Unknown'}</div>
-<div class="card__title card__title--primary">${song.opinion || 'none'}</div>
-
-<div class="card__actions">
-
-<button type="button" class="btn btn--flat orange--text" data-ripple="true">
-<div class="btn__content">Rating: ${song.rating}</div></button>
-
-<button type="button" class="btn btn--flat orange--text" data-ripple="true">
-<div class="btn__content">Explore</div></button>
-
-</div>
-
-</div></div></div></div>
-</div>
-</v-layout>
-</v-container>
-                            </article>
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK) {
+        const songs = window.JSON.parse(xhr.responseText).data;
+        console.log(songs);
+        if(songs.length === 0){
+          html = '<p><strong>There have not been any Songs added, <a href="/admin">go add some at /admin</a></strong></p>';
+        } else {
+          html = songs.map(function(song) {
+            return `
+    <!--<article>
+               <v-card-media src="/static/img/local-business.jpg" height="200px">
+                  </v-card-media>
+                  <v-card-title primary-title>
+                    <div>
+                      <h3 class="headline mb-0">${song.title || 'Unknown'} by ${song.artist || 'Unknown'}</h3>
+                      <div class="subheading grey--text text--darken-1">${song.opinion || 'none'}</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn flat color="teal">RATING: ${song.rating}</v-btn>
+                    <router-link to="/localwebsitedesign/"><v-btn flat color="teal">Explore</v-btn></router-link>
+    </article>-->
+      
+                  <article>
+                  <v-card-media src="/static/img/local-business.jpg" height="200px">
+                  </v-card-media>
+                  <v-card-title primary-title>
+                    <div>
+                      <h3 class="headline mb-0">Website Design3</h3>
+                      <div class="subheading grey--text text--darken-1">Google powered website specifically designed for local business that works across all devices and loads instantly.</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn flat color="teal">PRICE: $999</v-btn>
+                    <router-link to="/localwebsitedesign/"><v-btn flat color="teal">Explore</v-btn></router-link>
+                  </v-card-actions>
+                  </article>
                         `;
-                    }).join();
-                }
-            } else {
-                html = '<p><strong>The /api endpoint did not respond correctly :-(</strong></p>';
-            }
-
-            document.querySelector('#main').innerHTML = html;
+          }).join();
         }
-    }
+      } else {
+        html = '<p><strong>The /api endpoint did not respond correctly :-(</strong></p>';
+      }
 
-    document.addEventListener("DOMContentLoaded", loadHomepage);
+      document.querySelector('#main').innerHTML = html;
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", loadHomepage);
 })();
